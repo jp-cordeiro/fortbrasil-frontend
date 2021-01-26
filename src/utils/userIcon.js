e4a60d213ae9052709866db26f67533ca2d45e62
JP <jpc.araujo@hotmail.com>
2021-01-25 23:44:50 -0300

    add custom icons

---
+++ src/utils/userIcon.js
@@ -0,0 +1,12 @@
+import L from 'leaflet';
+import mapMarkerImg from '../assets/user-location.png';
+
+const userIcon = L.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60],
+});
+
+export default userIcon;

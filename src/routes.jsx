bd2a89b5f66ea0847fb241fa1361b0240474ebe8
JP <jpc.araujo@hotmail.com>
2021-01-26 01:10:52 -0300

    route for signUp

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,14 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import CreateEstablisment from './pages/Establishment/Establisment';
 import Dashboard from './pages/Dashboard/Dashboard';
 import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SingUp/SignUp';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={SignIn} />
+        <Route path="/novo-usuario" component={SignUp} />
         <Route path="/estabelecimentos/novo" component={CreateEstablisment} />
         <Route path="/estabelecimentos/:id" component={CreateEstablisment} />
         <Route path="/estabelecimentos" component={Dashboard} />
